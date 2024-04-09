60bdf8ed893613c48af0b172006a3960f6b845ea
parkchaerin <alicx991117@naver.com>
2024-04-09 13:57:22 +0900

    new project

---
+++ src/test/java/trafficMap/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package trafficMap.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

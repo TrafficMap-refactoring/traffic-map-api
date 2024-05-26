9846de738b3fa0c5af2988b37616a0be154839b2
parkchaerin <alicx991117@naver.com>
2024-05-26 15:31:54 +0900

    searchSubway GetMapping 생성

--- src/main/java/trafficMap/api/subway/subwayController.java
+++ src/main/java/trafficMap/api/subway/subwayController.java
@@ -1,7 +1,17 @@
 package trafficMap.api.subway;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/subway")
 public class subwayController {
+
+    @GetMapping
+    public String searchSubway(@RequestParam("name") String name){
+        return "test";
+    }
+
 }

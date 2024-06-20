ba35ea9c089512cdfa74891797364c43cdee6930
hwayoung <lweyine.sm@gmail.com>
2024-06-20 10:36:51 +0900

    feat BACK-026 버스도착예정정보 API

--- src/main/java/trafficMap/api/bus/BusService.java
+++ src/main/java/trafficMap/api/bus/BusService.java
@@ -1,6 +1,7 @@
 package trafficMap.api.bus;
 
 import org.springframework.stereotype.Service;
+import trafficMap.api.bus.busDto.BusArrInfoDto;
 import trafficMap.api.bus.busDto.BusStopDto;
 
 import java.util.List;
@@ -8,4 +9,5 @@ import java.util.List;
 @Service
 public interface BusService {
   List<BusStopDto> getBusStopList(String keyword);
+  List<BusArrInfoDto> getBusArrInfoList(String arsId);
 }
--- src/main/java/trafficMap/api/bus/BusServiceImpl.java
+++ src/main/java/trafficMap/api/bus/BusServiceImpl.java
@@ -3,12 +3,14 @@ package trafficMap.api.bus;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import trafficMap.api.bus.busDto.BusArrInfoDto;
 import trafficMap.api.bus.busDto.BusStopDto;
 
 import java.util.List;
@@ -50,4 +52,37 @@ public class BusServiceImpl implements BusService{
     }
     return null;
   }
+
+  private final static String BusArrInfo_URL = "http://ws.bus.go.kr/api/rest/stationinfo/getStationByUid";
+
+  public List<BusArrInfoDto> getBusArrInfoList(String arsId){
+    WebClient webClient = WebClient.builder()
+            .baseUrl(BusArrInfo_URL)
+            .defaultHeader("serviceKey", busApiKey)
+            .build();
+
+    ObjectMapper objectMapper = new ObjectMapper();
+    objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+    ResponseEntity<String> result = webClient.get()
+            .uri(uriBuilder -> uriBuilder.path("")
+                    .queryParam("arsId", arsId)
+                    .queryParam("resultType", "json")
+                    .build())
+            .retrieve()
+            .toEntity(String.class)
+            .block();
+
+    if(result != null && result.getBody() != null){
+      try{
+        BusArrInfoDto.BusArrInfoResponse res = objectMapper.readValue(result.getBody(), BusArrInfoDto.BusArrInfoResponse.class);
+        return res.getMsgBody().getItemList();
+      }catch(JsonProcessingException e){
+        e.printStackTrace();
+      }
+    }
+
+    return null;
+  }
+
 }

d64495b8d88f6fd574fe6d140be0392b77de5454
이화영 <101244382+lhy981113@users.noreply.github.com>
2024-06-21 21:22:06 +0900

    fix BusStopByRouteDto 수정

--- src/main/java/trafficMap/api/bus/busDto/BusStopByRouteDto.java
+++ src/main/java/trafficMap/api/bus/busDto/BusStopByRouteDto.java
@@ -15,7 +15,7 @@ public class BusStopByRouteDto {
   private String busRouteAbrv; //노선 약칭
   private String busRouteId; //노선 ID
   private String busRouteNm; //노선명
-  private String seq; //순번
+  private int seq; //순번
   private String section; //구간 ID
   private String station; //정류소 고유 ID
   private String stationNm; //정류소 이름
@@ -23,14 +23,14 @@ public class BusStopByRouteDto {
   private double gpsY; //Y좌표 (WGS84)
   private String direction; //진행 방향
   private String stationNo; //정류소 번호
-  private String routeType; //노선 유형
+  private int routeType; //노선 유형
   private String beginTm; //첫차 시간
   private String lastTm; //막차 시간
   private String trnstnid; //회차지 정류소 ID
   private double posX; //좌표X (GRS80)
   private double posY; //좌표Y (GRS80)
   private String arsId; //정류소 고유 번호
-  private String transYn; //회차지 여부
+  private char transYn; //회차지 여부
 
   @Getter
   @Setter

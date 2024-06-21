98fddce30c125c59340277671ad603c42e49e83e
이화영 <101244382+lhy981113@users.noreply.github.com>
2024-06-21 21:18:07 +0900

    fix BusInfoDto 수정

--- src/main/java/trafficMap/api/bus/busDto/BusInfoDto.java
+++ src/main/java/trafficMap/api/bus/busDto/BusInfoDto.java
@@ -13,11 +13,11 @@ public class BusInfoDto {
   private String busRouteAbrv; //노선 약칭
   private String busRouteId; //노선 ID
   private String busRoutedNm; //노선명
-  private String length; //노선 길이
-  private String routeType; //노선 유형
+  private int length; //노선 길이
+  private int routeType; //노선 유형
   private String stStationNm; //기점
   private String edStationNm; //종점
-  private String term; //배차 간격(분)
+  private int term; //배차 간격(분)
   private String lastBustYn; //막차 운행 여부
   private String firstBusTm; //금일 첫차 시간
   private String lastBusTm; //금일 막차 시간

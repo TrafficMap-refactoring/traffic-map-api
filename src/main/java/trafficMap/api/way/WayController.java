06bc52561078745e7297938ad3a0573d2a1aabf3
parkchaerin <alicx991117@naver.com>
2024-06-23 16:08:29 +0900

    way/draw api 추가

--- src/main/java/trafficMap/api/way/WayController.java
+++ src/main/java/trafficMap/api/way/WayController.java
@@ -1,6 +1,7 @@
 package trafficMap.api.way;
 
 import lombok.AllArgsConstructor;
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,5 +29,11 @@ public class WayController {
     @ResponseBody
     public String FindTransWay4(String sName,String eName){ // 카카오 대중교통 길찾기 연결 -> 출발지, 도착지 이름 or 주소 입력하는 방법
         return wayService.findTransWay(sName, eName);
+    }
+
+    @GetMapping("/draw")
+    @ResponseBody
+    public JSONObject FindDrawWay(double startX, double startY, double endX, double endY, String startName, String endName, Number option){
+        return wayService.findDrawWay(startX, startY, endX, endY, startName, endName, option);
     }
 }
